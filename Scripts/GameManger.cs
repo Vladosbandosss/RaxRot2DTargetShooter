8298a32dd7248abeb6706ad9c0c86873c3c36e9d
Vladosbandosss <dasistperfektos@gmail.com>
2021-09-03 16:06:44 +0300

    Game
    ReadyGame

---
+++ Scripts/GameManger.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManger : MonoBehaviour
+{
+    public Button restbutton;
+    new AudioSource audio;
+    public GameObject target;
+    private float maxXpos = 8f;
+    private float maxYpos = 4f;
+    public Text scoreText;
+    int score = 0;
+
+    bool win = false;
+    public GameObject winText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // restbutton = GetComponent<Button>();
+        Debug.Log("заое");
+        InvokeRepeating(nameof(Spawn), 1f, 2f);
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (win)
+        {
+            CancelInvoke(nameof(Spawn));
+           
+        }
+    }
+    void Spawn()
+    {
+        
+        float randomX = Random.Range(-maxXpos, maxYpos);
+        float randomY = Random.Range(-maxYpos, maxYpos);
+        Vector3 randomPos = new Vector3(randomX, randomY, 0);
+      Instantiate(target, randomPos, Quaternion.identity);
+    }
+
+   public void IncrementScore()
+    {
+        // GetComponent<AudioSource>().Play();
+        audio.Play();
+        score++;
+        scoreText.text = score.ToString();
+
+        if (score >= 10)
+        {
+            win = true;
+            winText.SetActive(true);
+           restbutton.gameObject.SetActive(true);
+        }
+    }
+
+    public void RestarGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
---
+++ Scripts/Tatget.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tatget : MonoBehaviour
+{
+
+    public GameManger gameManger;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        gameManger = GameObject.Find("GameManager").GetComponent<GameManger>();
+        Destroy(gameObject, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        gameManger.IncrementScore();
+       
+        Destroy(gameObject);
+    }
+}
